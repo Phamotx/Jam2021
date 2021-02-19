36122894d33aa615ec8c7987a3ea00a4b49922b6
Manoj <chauhanmnaoj659@gmail.com>
2021-02-19 15:40:39 +0530

    1st Commit

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    [SerializeField]
+    private float _gravity = 9.81f;
+    [SerializeField]
+    private float _jumpSpeed = 3.5f;
+    [SerializeField]
+    private float _doubleJumpMultiplier = 0.5f;
+
+    [SerializeField]
+    private Animator anime;
+
+    private CharacterController _controller;
+
+    private float _directionY;
+
+    private bool _canDoubleJump = false;
+    private bool _wallJump = false;
+    Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (_controller.isGrounded)
+        {
+            direction = new Vector3(0, 0, horizontalInput);
+            if (direction.z != 0)
+            {
+                transform.right = Vector3.Slerp(transform.right, Vector3.right * direction.z, 10f);
+                anime.SetBool("Run", true);
+            }
+            else
+            {
+                anime.SetBool("Run", false);
+            }
+
+            _canDoubleJump = true;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //  anime.SetBool("Jump", true);
+                _directionY = _jumpSpeed;
+                _directionY -= _gravity * Time.deltaTime;
+            }
+            else
+            {
+                anime.SetBool("Jump", false);
+            }
+        }
+        else
+        {
+            Debug.LogError("Player Falling");
+            anime.SetBool("Jump", true);
+            if (Input.GetButtonDown("Jump") && _canDoubleJump && !_wallJump)
+            {
+                _directionY = _jumpSpeed * _doubleJumpMultiplier;
+                _canDoubleJump = false;
+            }
+            _directionY -= _gravity * Time.deltaTime;
+        }
+
+
+
+        direction.y = _directionY;
+
+        _controller.Move(direction * _moveSpeed * Time.deltaTime);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (!_controller.isGrounded && hit.normal.y < 0.1f && hit.collider.tag == "Wall")
+        {
+            _wallJump = true;
+            if (Input.GetButtonDown("Jump") && _wallJump)
+            {
+                _wallJump = false;
+                // anime.SetBool("Jump", true);
+                _directionY = _jumpSpeed;
+                Debug.LogError(hit.normal);
+                direction = hit.normal;
+                transform.right = Vector3.Slerp(transform.right, Vector3.right * direction.z, 10f);
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/EventManger.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Event
+{
+}
+public class EventManger : MonoBehaviour
+{
+    private static EventManger eventManger;
+
+    public static EventManger EventMangers
+    {
+        get
+        {
+            eventManger = FindObjectOfType<EventManger>() as EventManger;
+            return eventManger;
+        }
+    }
+    Dictionary<Event, List<Action<EventInfo>>> eventListeners;
+
+    public void SubScribeEvent(Event eventName,Action<EventInfo> method)
+    {
+        if (eventListeners == null)
+        {
+            eventListeners = new Dictionary<Event, List<Action<EventInfo>>>();
+        }
+        if(!eventListeners.ContainsKey(eventName)|| eventListeners[eventName] == null)
+        {
+            eventListeners.Add(eventName, new List<Action<EventInfo>>());
+            eventListeners[eventName].Add(method);
+        }
+        eventListeners[eventName].Remove(method);
+        eventListeners[eventName].Add(method);
+        Debug.Log("Event Get Scribed");
+    }
+    public void DeSubScribeEvent(Event eventName, Action<EventInfo> method)
+    {
+        if (!eventListeners.ContainsKey(eventName) || eventListeners[eventName] == null)
+        {
+            Debug.Log("Such Kind of Event does not exits");
+            return;
+        }
+        eventListeners[eventName].Remove(method);
+        Debug.Log("Event Get DeScribed");
+    }
+
+    public void TriggerEvent(Event eventName, EventInfo door)
+    {
+        if (!eventListeners.ContainsKey(eventName) || eventListeners[eventName] == null)
+        {
+            Debug.Log("Such Kind of Method does not exits on this event");
+            return;
+        }
+        foreach (var el in eventListeners[eventName])
+        {
+            el(door);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        eventListeners.Clear();
+        Debug.Log("All Event Get DeScribed");
+    }
+}
